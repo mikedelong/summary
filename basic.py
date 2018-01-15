58512e4184f97e8079e472602efb19ed996b9be1
Mike DeLong <mikedelong@outlook.com>
2018-01-14 19:01:43 -0500

    feature: a first cut at a working prototype

--- basic.py
+++ basic.py
@@ -1,2 +1,18 @@
 # https://hackernoon.com/how-to-run-text-summarization-with-tensorflow-d4472587602d
 
+import struct
+
+from tensorflow.core.example import example_pb2
+
+output_filename = './output.txt'
+with open(output_filename, 'wb') as writer:
+    body = 'body'
+    title = 'title'
+
+    tf_example = example_pb2.Example()
+    tf_example.features.feature['article'].bytes_list.value.extend([body])
+    tf_example.features.feature['abstract'].bytes_list.value.extend([title])
+    tf_example_str = tf_example.SerializeToString()
+    str_len = len(tf_example_str)
+    writer.write(struct.pack('q', str_len))
+writer.write(struct.pack('%ds' % str_len, tf_example_str))
